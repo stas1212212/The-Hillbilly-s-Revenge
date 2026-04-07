95e918dbe7840c4e83adb1e3469a762bc2e9b9d9
golakailla6-web <golakailla6@gmail.com>
2026-04-07 23:29:28 +0300

    сейф 3

--- the-hillbilly's-revenge/menu.gd
+++ the-hillbilly's-revenge/menu.gd
@@ -4,3 +4,8 @@ extends Node2D
 func _on_quit_pressed() -> void:
 	pass # Replace with function body.
 	get_tree() .quit()
+
+
+
+func _on_play_pressed() -> void:
+	get_tree().change_scene_to_file("res://level.tscn")
